faf484478cf71f622247ede9ad7428eeabcedfa2
Ahmed Bilal Bhatti <110194946+AhmedBilalSSG@users.noreply.github.com>
2023-07-20 16:06:31 +0500

    Update settings.py

--- Graph/settings.py
+++ Graph/settings.py
@@ -170,4 +170,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'ahmadbilalssg@gmail.com'
-EMAIL_HOST_PASSWORD = 'jkfwaisisezpkmmr'
+EMAIL_HOST_PASSWORD = ''
